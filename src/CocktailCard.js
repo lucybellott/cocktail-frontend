9152d92f548c6f70af7dee65fad63be67ea0a294
Daniel Mueller <danielpetermueller@gmail.com>
2021-06-22 17:33:49 -0400

    completed MVP

--- src/CocktailCard.js
+++ src/CocktailCard.js
@@ -29,13 +29,35 @@ export default function CocktailCard({id, image, name, description, likes, ingre
     const handleClick = (e) => {
         setClick(!isClicked)
     }
+
+    const [edit, setEdit] = useState(false)
+    const [newDescription, setNewDescription] = useState(description)
+    
+    function editDescription(){
+        setEdit(edit => !edit)
+    }
+
+    function saveDescription(e){
+        e.preventDefault()
+        fetch (`http://localhost:3000/drinks/${id}`,{
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "description": newDescription
+            })
+        })
+        .then(editDescription())
+    }
     
     return (
         <div class="card" style={{width: "15rem", overflow:"hidden", boxShadow: '3px 5px 1px 1px #00000034'}}>
         <img src={image} class="card-img-top" alt="..." style={{width:'15rem'}}/>
         <div class="card-body">
         <h5 class="card-title">{name}</h5>
-        <p style={{fontStyle:"italic"}}class="card-text">{description}</p>
+        {edit ? <><form onSubmit={saveDescription}><input onChange={(e) => setNewDescription(e.target.value)} value={newDescription} type="text"></input><button type="submit" class="btn btn-dark">Save</button></form></>:<><p style={{fontStyle:"italic"}}class="card-text">{newDescription}</p> <button onClick={editDescription} type="button" class="btn btn-dark">Edit</button></>}
+        
         <hr/>
        
         { isClicked ?
--- src/Homepage.js
+++ src/Homepage.js
@@ -33,7 +33,7 @@ function Homepage() {
   return (
     <div className="App">
       <Search search={search} setSearch={setSearch} />
-      {drinks.length > 0 ? <CocktailList drinks={searchCocktails()} /> : <h1>Loading...</h1>}
+      {drinks.length > 0 ? <CocktailList drinks={searchCocktails()} /> : <img src="https://img.pikbest.com/58pic/35/39/61/62K58PICb88i68HEwVnm5_PIC2018.gif" />}
       
 
     </div>
