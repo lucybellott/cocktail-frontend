831f3c53f1c6867861ed597df02713f5a2695633
Lucy <lucybellott@gmail.com>
2021-08-30 18:11:05 -0400

    banner

--- src/CocktailList.js
+++ src/CocktailList.js
@@ -5,7 +5,8 @@ export default function CocktailList({ drinks }) {
 
     const drinksArray = drinks.map((drink) => {
         return <>
-            <div className="col-4" style={{ marginBottom: '20px',  }}><CocktailCard
+            <div className="col-4" style={{ marginBottom: '20px',  }}>
+                <CocktailCard
                 key={drink.id}
                 {...drink}
             />
